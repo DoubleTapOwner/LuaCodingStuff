176364812de4db391812cb3e40b283507586a9e9
RippleHubOwner <109435058+DoubleTapOwner@users.noreply.github.com>
2022-08-07 14:36:36 +0100

    Update ClickerMadness.lua

--- ClickerMadness.lua
+++ ClickerMadness.lua
@@ -1,16 +1,16 @@
-local mod = require(game:GetService("Players").PUTURUSERHERE.PlayerScripts.Aero.Controllers.UI.Pets) -- put your user their 
+local mod = require(game:GetService("Players").LocalPlayer.PlayerScripts.Aero.Controllers.UI.Pets) 
 setconstant(mod.CalculatePetCapacity, 7, 700)
 
-getgenv().autoTap = false; -- set these to true to work
-getgenv().autoRebirth = false; -- set these to true to work
-getgenv().BuyEgg = false; -- set these to true to work
+getgenv().autoTap = false 
+getgenv().autoRebirth = false 
+getgenv().BuyEgg = false 
 
-local remotePath = game:GetService("ReplicatedStorage").Aero.AeroRemoteServices;
+local remotePath = game:GetService("ReplicatedStorage").Aero.AeroRemoteServices
 
-local clickMod = require(game:GetService("Players")["VictoriaHarris27"].PlayerScripts.Aero.Controllers.UI.Click)
+local clickMod = require(game:GetService("Players").LocalPlayer.PlayerScripts.Aero.Controllers.UI.Click)
 
 function doTap()
-    spawn(function() 
+    spawn(function()
         while getgenv().autoTap == true do
             clickMod:Click()
             wait()
@@ -19,7 +19,7 @@ function doTap()
 end
 
 function autoRebirth(rebirthAmount)
-    spawn(function() 
+    spawn(function()
         while getgenv().autoRebirth == true do
             remotePath.RebirthService.BuyRebirths:FireServer(rebirthAmount)
             wait()
@@ -30,24 +30,26 @@ end
 function buyEgg(eggType)
     spawn(function()
         while wait() do
-            if not getgenv().BuyEgg then break end;
+            if not getgenv().BuyEgg then
+                break
+            end
             remotePath.EggService.Purchase:FireServer(eggType)
         end
     end)
 end
 
 function getCurrentPlayerPOS()
-    local plyr = game.Players.LocalPlayer;
+    local plyr = game.Players.LocalPlayer
     if plyr.Character then
-        return plyr.Character.HumanoidRootPart.Position;
+        return plyr.Character.HumanoidRootPart.Position
     end
-    return false;
+    return false
 end
 
 function teleportTO(placeCFrame)
-    local plyr = game.Players.LocalPlayer;
+    local plyr = game.Players.LocalPlayer
     if plyr.Character then
-        plyr.Character.HumanoidRootPart.CFrame = placeCFrame;
+        plyr.Character.HumanoidRootPart.CFrame = placeCFrame
     end
 end
 
@@ -57,69 +59,71 @@ function teleportWorld(world)
     end
 end
 
-local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
+local library =
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3"))()
 
-local w = library:CreateWindow("Clicker Madness") 
+local w = library:CreateWindow("Clicker Madness")
 
 local a = w:CreateFolder("Farming")
 
-local b = w:CreateFolder("Pet") 
+local b = w:CreateFolder("Pet")
 
-local c = w:CreateFolder("Teleports") 
+local c = w:CreateFolder("Teleports")
 
 a:DestroyGui()
 
-a:Button("RebirthGamepass",function()
+a:Button("RebirthGamepass", function()
     function unlockGamepasses()
         local gamepassMod = require(game:GetService("ReplicatedStorage").Aero.Shared.Gamepasses)
-        gamepassMod.HasPassOtherwisePrompt = function() return true end
-    end
-end)
-  
-a:Button("InventorySpace",function()
-    local mod = require(game:GetService("Players").VictoriaHarris27.PlayerScripts.Aero.Controllers.UI.Pets)
-        setconstant(mod.CalculatePetCapacity, 7, 700)
+        gamepassMod.HasPassOtherwisePrompt = function()
+            return true
+        end
     end
 end)
 
-a:Toggle("Auto Tap",function(bool)
+a:Button("InventorySpace", function()
+    local mod = require(game:GetService("Players").VictoriaHarris27.PlayerScripts.Aero.Controllers.UI.Pets)
+    setconstant(mod.CalculatePetCapacity, 7, 700)
+end)
+
+a:Toggle("Auto Tap", function(bool)
     getgenv().autoTap = bool
-    print("Auto Tap is:", bool);
+    print("Auto Tap is:", bool)
     if bool then
-        doTap();
+        doTap()
     end
 end)
 
 local selectedRebirth
-a:Dropdown("Rebirth Amount",{"1","10","100","1000","10000","100000"},true,function(value) 
-    selectedRebirth = value;    
+a:Dropdown("Rebirth Amount", { "1", "10", "100", "1000", "10000", "100000" }, true, function(value)
+    selectedRebirth = value
     print(value)
-end)    
+end)
 
-a:Toggle("Auto Rebirth",function(bool)
+a:Toggle("Auto Rebirth", function(bool)
     getgenv().autoRebirth = bool
-    print("Auto Rebirth is:", bool);
+    print("Auto Rebirth is:", bool)
     if bool and selectedRebirth then
-        autoRebirth(selectedRebirth);
+        autoRebirth(selectedRebirth)
     end
 end)
 
-b:Toggle("Auto Buy Pet",function(bool)
+b:Toggle("Auto Buy Pet", function(bool)
     getgenv().autoRebirth = bool
-    print("Auto Buy Pet is:", bool);
+    print("Auto Buy Pet is:", bool)
     if bool then
-        buyEgg("basic");
+        buyEgg("basic")
     end
 end)
 
-local selectedWorld;
+local selectedWorld
 
-c:Dropdown("World's",{"Lava","Desert","Ocean","Winter","Toxic"},true,function(value) 
-    selectedWorld = value;    
+c:Dropdown("World's", { "Lava", "Desert", "Ocean", "Winter", "Toxic" }, true, function(value)
+    selectedWorld = value
     print(value)
-end)    
-     
-c:Button("Teleport Selected",function()
+end)
+
+c:Button("Teleport Selected", function()
     if selectedWorld then
         teleportWorld(selectedWorld)
     end
